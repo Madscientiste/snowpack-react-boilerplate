364c05d077ce38125de0b374973d441104e0ae7c
Madscientist <s.shaade77@gmail.com>
2021-02-23 15:54:59 +0100

    EOL change from LF to CRLF

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App"
+import App from "./App";
 
-import './styles/main.scss'
-import "@fortawesome/fontawesome-free/js/all"
+import "./styles/main.scss";
+import "@fortawesome/fontawesome-free/js/all";
 
 ReactDOM.render(
     <React.StrictMode>
